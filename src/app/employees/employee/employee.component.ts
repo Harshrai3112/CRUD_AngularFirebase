f203829e4885bfdcfd7f591c8715ebb0feef55ab
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2019-05-06 17:02:39 +0530

    First Commit

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -3,6 +3,7 @@ import { EmployeeService } from 'src/app/shared/employee.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { NgForm } from '@angular/forms';
+import { Employee } from 'src/app/shared/employee.model';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -13,21 +14,43 @@ export class EmployeeComponent implements OnInit {
   empCode = '';
   position = '';
   data = {};
-  emp: any;
+  emp = [];
+  update = [];
   constructor(private service: EmployeeService, private firestore: AngularFirestore) {
   }
   submit() {
     this.data = Object.assign({}, { name: this.name, empCode: this.empCode, position: this.position });
     this.firestore.collection('employees').add(this.data);
-    console.log(this.name, this.data);
+    this.update.push(false);
+    this.name = '';
+    this.empCode = '';
+    this.position = '';
   }
   getEmp() {
     return this.firestore.collection('employees').snapshotChanges().subscribe(actionArray => {
-      this.emp = actionArray.map(item => {
-        console.log(item.payload.doc.data());
+     this.emp = actionArray.map(item => {
+       return {
+        id: item.payload.doc.id,  
+        ...item.payload.doc.data()} as Employee;
       });
     });
   }
+  updateEmp(id,i){
+    this.firestore.doc('employees/' + id).update({name: this.name,position: this.position,
+    empCode: this.empCode});
+    this.update[i] = false ;
+    this.name = '';
+    this.empCode = '';
+    this.position = '';
+  }
+  upd(i){
+    this.update[i] = true;
+  }
+  deleteEmp(id){
+    this.firestore.doc('employees/' + id).delete();
+    this.update.pop();
+  }
+
   ngOnInit() {
   }
 
